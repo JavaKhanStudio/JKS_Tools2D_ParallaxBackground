e082428da18b26b8f2f996e3846d05ed4e9ccac9
Simon Bédard <JavaKhanStudio@Gmail.com>
2022-08-17 02:57:16 +0200

    Screen shot method start

---
+++ editor/src/jks/tools2d/gifsaver/ScreenshotFactory.java
@@ -0,0 +1,65 @@
+package jks.tools2d.gifsaver;
+
+import java.io.File;
+import java.nio.ByteBuffer;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.files.FileHandle;
+import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.graphics.PixmapIO;
+import com.badlogic.gdx.utils.ScreenUtils;
+
+public class ScreenshotFactory 
+{
+	private static int counter = 1;
+	private static int time = 50;//times
+	private static float df = 0.1f;//interval
+	
+	private static void saveScreenshot_ex() 
+	{
+		try {
+			FileHandle fh;
+			do {
+				new File(Gdx.files.getExternalStoragePath() + "save").mkdir();
+				fh = new FileHandle(Gdx.files.getExternalStoragePath()
+						+ "save/" + "screenshot" + counter++ + ".png");
+			} while (fh.exists());
+			Pixmap pixmap = getScreenshot(0, 0, Gdx.graphics.getWidth(),
+					Gdx.graphics.getHeight(), true);
+			PixmapIO.writePNG(fh, pixmap);
+			pixmap.dispose();
+		} catch (Exception e) {
+		}
+	}
+	
+	private static Pixmap getScreenshot(int x, int y, int w, int h,
+			boolean yDown) {
+		final Pixmap pixmap = ScreenUtils.getFrameBufferPixmap(x, y, w, h);
+		if (yDown) {
+			//Flip the pixmap upside down
+			ByteBuffer pixels = pixmap.getPixels();
+			int numBytes = w * h * 4;
+			byte[] lines = new byte[numBytes];
+			int numBytesPerLine = w * 4;
+			for (int i = 0; i < h; i++) {
+				pixels.position((h - i - 1) * numBytesPerLine);
+				pixels.get(lines, i * numBytesPerLine, numBytesPerLine);
+			}
+			pixels.clear();
+			pixels.put(lines);
+		}
+		return pixmap;
+	}
+	
+	public static void saveScreenshot(float delta) 
+	{
+		if (time < 50) 
+		{
+			if (df > 0.1f) 
+			{
+				time += 1;
+				df = 0;
+				saveScreenshot_ex();
+			}
+		}
+	}
+}
